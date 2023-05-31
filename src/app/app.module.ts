941334e20bbdee3e4811b4d4a4cbb28ef97263f0
RobertoStagg <stagg.roberto@gmail.com>
2023-05-31 09:18:44 -0700

    Added sign up and forgot password components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientManagementComponent } from './client-management/client-management.component';
 import { ClientMeetingManagementComponent } from './client-meeting-management/client-meeting-management.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+const routes: Routes = [
+  { path: 'client-management', component: ClientManagementComponent },
+  { path: 'client-meeting-management', component: ClientMeetingManagementComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientManagementComponent,
-    ClientMeetingManagementComponent
+    ClientMeetingManagementComponent,
+    SignUpComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
